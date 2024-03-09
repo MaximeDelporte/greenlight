7e348ef337fcdefd2ac4f499592869d62a36b3ae
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-03-09 15:59:41 +0100

    Create writeJSON inside helpers.go and use it inside healthcheck.go. This way, we can writeJSON more easily for the next endpoints.

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -12,18 +11,9 @@ func (app application) healthCheckHandler(w http.ResponseWriter, r *http.Request
 		"version":     version,
 	}
 
-	js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
+		http.Error(w, "The server encountered a problem and could not processs your request", http.StatusInternalServerError)
 	}
-
-	// Append a newline to the JSON.
-	// This is just a small nicely to make it easier to view in terminal applications.
-	js = append(js, '\n')
-
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write(js)
 }
